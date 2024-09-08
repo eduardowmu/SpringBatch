f4851c19a8c548dd05542744981d5fdfac0a3ad2
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-09-07 22:22:56 -0300

    agrupando steps com flows

---
+++ springbatch-flows-main/src/main/java/br/com/giulianabezerra/sbflows/SbFlowsApplication.java
@@ -0,0 +1,13 @@
+package br.com.giulianabezerra.sbflows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFlowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFlowsApplication.class, args);
+	}
+
+}
